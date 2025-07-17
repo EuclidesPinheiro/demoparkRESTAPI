72cdddaa55168fb70864fc24dd9cb9a8b872b9d5
euclides.pereira <euclides.pereira@dataprev.gov.br>
2025-07-17 19:31:48 -0300

    first commit to start

---
+++ src/main/java/com/epinheiro/demo_park_api/config/SpringTimezoneConfig.java
@@ -0,0 +1,16 @@
+package com.epinheiro.demo_park_api.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class SpringTimezoneConfig {
+    @PostConstruct // essa anotação faz com que o mét0do seja executado após a construção do bean
+    public void timezoneConfig() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
+
+
+}
---
+++ src/test/java/com/epinheiro/demo_park_api/DemoParkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epinheiro.demo_park_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoParkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
